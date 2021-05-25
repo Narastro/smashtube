9df3016d8e6cde55f21b09167eae4b17ecfab0ac
Narastro <78056880+Narastro@users.noreply.github.com>
2021-05-25 18:48:05 +0900

    learn about server

---
+++ src/test.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+
+app.get("/",(req,res)=>res.send("<h1>hello! welcome!</h1>"))
+
+app.get("/about",(req,res)=>res.send("<h1>hello! welcome!</h1>"))
+
+app.get("/contact",(req,res)=>res.send("<h1>hello! welcome!</h1>"))
+
+app.get("/login",(req,res)=>res.send("<h1>hello! welcome!</h1>"))
+
