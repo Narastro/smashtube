f899305bdd96d83e05cecf8ba2be53bbe8076466
Narastro <advice02@naver.com>
2021-06-08 20:08:10 +0900

    findByIdAndUpdate

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,38 +1,79 @@
 import Video from "../models/Video";
 
-export const home = (req, res) => {
-  Video.find({});//DB
-  res.render("home", { pageTitle: "Home", videos: [] });
+export const home = async (req, res) => {
+  try {
+    const videos = await Video.find({});
+    res.render("home", { pageTitle: "Home", videos });
+  } catch (error) {
+    return res.render("Server-Error", error);
+  }
 };
 export const search = (req, res) => res.send("Search!");
 export const upload = (req, res) => res.send("Upload!");
-export const watch = (req, res) => {
+
+export const watch = async (req, res) => {
+  const { id } = req.params;
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  }
   return res.render("watch", {
-    pageTitle: `Watching : `,
+    pageTitle: video.title,
+    video,
   });
 };
-export const getEdit = (req, res) => {
+
+export const getEdit = async (req, res) => {
+  const { id } = req.params;
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  }
   return res.render("edit", {
-    pageTitle: `Editing : `,
+    pageTitle: `Edit: ${video.title}`,
+    video,
   });
 };
-export const postEdit = (req, res) => {
+
+export const postEdit = async (req, res) => {
+  const { id } = req.params;
+  const { title, description, hashtags } = req.body;
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  }
+  await Video.findByIdAndUpdate(id, {
+    title,
+    description,
+    hashtags: hashtags
+      .split(",")
+      .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+  });
   return res.redirect(`/videos/${id}`);
 };
+
 export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
-export const postUpload = (req, res) => {
-  const newVideo = {
-    title: req.body.title,
-    rating: 0,
-    comments: 0,
-    createAt: "Just now",
-    view: 0,
-    id: videos.length + 1,
-  };
-  videos.push(newVideo);
-  return res.redirect("/");
+export const postUpload = async (req, res) => {
+  try {
+    const { title, description, hashtags } = req.body;
+    const video = new Video({
+      title,
+      description,
+      createAt: Date.now(),
+      hashtags: hashtags.split(",").map((word) => `#${word}`),
+      meta: { view: 0, rating: 0 },
+    });
+    await video.save();
+    return res.redirect("/");
+  } catch (error) {
+    console.log(error);
+    res.render("upload", {
+      pageTitle: "Upload Video",
+      errorMessage: error._message,
+    });
+  }
 };
 
 export const deleteVideo = (req, res) => res.send("Delete Video!");
