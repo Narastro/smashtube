806fcd043a74f44e2c4299e65e60e99689d6d4a9
Narastro <advice02@naver.com>
2021-06-18 18:41:55 +0900

    Webpack modified

--- src/middlewares.js
+++ src/middlewares.js
@@ -3,7 +3,7 @@ import multer from "multer";
 export const localsmiddleware = (req, res, next) => {
   res.locals.siteName = "Smashtube";
   res.locals.loggedIn = Boolean(req.session.loggedIn);
-  res.locals.loggedInUser = req.session.user;
+  res.locals.loggedInUser = req.session.user || {};
   next();
 };
 
