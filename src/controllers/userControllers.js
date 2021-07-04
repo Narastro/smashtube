dd62c3ceab15878c9d343d75ba864da05d714c59
Narastro <advice02@naver.com>
2021-07-04 22:25:43 +0900

    Bug fixed

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -96,7 +96,7 @@ export const postEdit = async (req, res) => {
   const updateUser = await User.findByIdAndUpdate(
     _id,
     {
-      avatarUrl: file ? `/${file.location}` : avatarUrl,
+      avatarUrl: file ? file.location : avatarUrl,
       name,
       email,
       username,
